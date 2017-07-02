5ff876acd69bbccf884f157551f2f3dd5b30e0b2
Miriam Rozenbaum <miriamrozenbaum@Miriams-MacBook-Pro.local>
2017-07-01 21:40:01 -0500

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    
-]
+    url(r'^blog/',include('blog.urls')), 
+    url(r'^$', RedirectView.as_view(url='/blog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
