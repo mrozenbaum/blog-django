7412de960bf03ff8fcfce39153349be349d1fee4
Miriam Rozenbaum <miriamrozenbaum@Miriams-MacBook-Pro.local>
2018-02-25 15:03:37 -0500

    added some comments

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zo%m!5s3ho^r054q_rvjs6wfp=q+13szat0&!2j&8ojmmj+cud'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -119,5 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
