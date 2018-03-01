3b9db3d1d76ceb8ab19351485f5ae715c836fba4
Miriam Rozenbaum <miriamrozenbaum@miriams-mbp.attlocal.net>
2018-03-01 15:44:28 -0500

    user can login,logout,name is displayed when logged in

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,6 +24,7 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     # url(r'', include('blog.urls')),
     url(r'^blog/',include('blog.urls')), 
     url(r'^$', RedirectView.as_view(url='/blog/', permanent=True)),
