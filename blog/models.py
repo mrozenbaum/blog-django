169c391325df47bf8985b7c8d98ff4795cea457d
Miriam Rozenbaum <miriamrozenbaum@miriams-mbp.attlocal.net>
2018-03-01 16:42:06 -0500

    comments can be approved, denied

--- blog/models.py
+++ blog/models.py
@@ -36,8 +36,12 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
+
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
